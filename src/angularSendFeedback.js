8191d4ec95b5a982093f8d8bb7fcfbeab30c145e
Dmitry Fontanov <dfontanov@odin.com>
2016-04-11 13:49:24 +0300

    Refactored additional info

--- src/angularSendFeedback.js
+++ src/angularSendFeedback.js
@@ -40,7 +40,7 @@ angular.module('angular-send-feedback').directive('angularFeedback', [
                 tpl: {
                   description:    '<div id="feedback-welcome"><div class="feedback-logo">Feedback</div><p>Feedback lets you send us suggestions about our products. We welcome problem reports, feature ideas and general comments.</p><p>Start by writing a brief description:</p><textarea id="feedback-note-tmp"></textarea><p>Next we\'ll let you identify areas of the page related to your description.</p><button id="feedback-welcome-next" class="feedback-next-btn feedback-btn-gray">Next</button><div id="feedback-welcome-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
                   highlighter:    '<div id="feedback-highlighter"><div class="feedback-logo">Feedback</div><p>Click and drag on the page to help us better understand your feedback. You can move this dialog if it\'s in the way.</p><button class="feedback-sethighlight feedback-active"><div class="ico"></div><span>Highlight</span></button><label>Highlight areas relevant to your feedback.</label><button class="feedback-setblackout"><div class="ico"></div><span>Black out</span></button><label class="lower">Black out any personal information.</label><div class="feedback-buttons"><button id="feedback-highlighter-next" class="feedback-next-btn feedback-btn-gray">Next</button><button id="feedback-highlighter-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div class="feedback-wizard-close"></div></div>',
-                  overview:       '<div id="feedback-overview"><div class="feedback-logo">Feedback</div><div id="feedback-overview-description"><div id="feedback-overview-description-text"><h3>Description</h3><h3 class="feedback-additional">Additional info</h3><div id="feedback-additional-none"><span>None</span></div><div id="feedback-browser-info"><span>Browser Info</span></div><div id="feedback-page-info"><span>Page Info</span></div><div id="feedback-page-structure"><span>Page Structure</span></div></div></div><div id="feedback-overview-screenshot"><h3>Screenshot</h3></div><div class="feedback-buttons"><button id="feedback-submit" class="feedback-submit-btn feedback-btn-blue">Submit</button><button id="feedback-overview-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div id="feedback-overview-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
+                  overview:       '<div id="feedback-overview"><div class="feedback-logo">Feedback</div><div id="feedback-overview-description"><div id="feedback-overview-description-text"><h3>Description</h3><p>We will also send the following information:</p><ul id="feedback-additional-info"></ul></div></div><div id="feedback-overview-screenshot"><h3>Screenshot</h3></div><div class="feedback-buttons"><button id="feedback-submit" class="feedback-submit-btn feedback-btn-blue">Submit</button><button id="feedback-overview-back" class="feedback-back-btn feedback-btn-gray">Back</button></div><div id="feedback-overview-error">Please enter a description.</div><div class="feedback-wizard-close"></div></div>',
                   submitSuccess:  '<div id="feedback-submit-success"><div class="feedback-logo">Feedback</div><p>Thank you for your feedback. We value every piece of feedback we receive.</p><p>We cannot respond individually to every one, but we will use your comments as we strive to improve your experience.</p><button class="feedback-close-btn feedback-btn-blue">OK</button><div class="feedback-wizard-close"></div></div>',
                   submitError:    '<div id="feedback-submit-error"><div class="feedback-logo">Feedback</div><p>Sadly an error occured while sending your feedback. Please try again.</p><button class="feedback-close-btn feedback-btn-blue">OK</button><div class="feedback-wizard-close"></div></div>'
                 },
@@ -52,6 +52,14 @@ angular.module('angular-send-feedback').directive('angularFeedback', [
               var supportedBrowser = !!window.HTMLCanvasElement;
               var isFeedbackButtonNative = settings.feedbackButton == '.feedback-btn';
               var html2canvas = settings.html2canvas;
+
+              function additionalInfo(text) {
+                var p = $('<p>').text(text);
+
+                $('#feedback-additional-info')
+                  .append($('<li>').append(p));
+              }
+
               if (supportedBrowser) {
                 if(isFeedbackButtonNative) {
                   $('body').append('<button class="feedback-btn feedback-btn-gray">' + settings.initButtonText + '</button>');
@@ -155,20 +163,32 @@ angular.module('angular-send-feedback').directive('angularFeedback', [
                     post.browser.userAgent      = navigator.userAgent;
                     post.browser.plugins        = [];
 
+                    function formatBrowserInfo(b) {
+                      return 'User agent: ' + b.userAgent + '\n' +
+                        'Platform: ' + b.platform;
+                    }
+
                     $.each(navigator.plugins, function(i) {
                       post.browser.plugins.push(navigator.plugins[i].name);
                     });
-                    $('#feedback-browser-info').show();
+
+                    additionalInfo('Your browser information');
                   }
 
                   if (settings.postURL) {
                     post.url = document.URL;
-                    $('#feedback-page-info').show();
+
+                    function formatPageInfo(url) {
+                      return 'URL: ' + url;
+                    }
+
+                    additionalInfo('The URL you are now at');
                   }
 
                   if (settings.postHTML) {
                     post.html = $('html').html();
-                    $('#feedback-page-structure').show();
+
+                    additionalInfo('The contents of this page');
                   }
 
                   if (!settings.postBrowserInfo && !settings.postURL && !settings.postHTML)
